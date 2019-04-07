bc11d1f0486df2f7029105100dd3f9f6c8875dc9
Stefaan Ponnet <stefaan@ponnet.com>
2019-04-07 20:30:03 +0200

    first visualisation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { Component } from "react";
+import logo from "./logo.svg";
+// import './App.css';
+import "bulma/css/bulma.css";
+// import { BrowserRouter, Route, Switch, Link } from "react-router-dom";
+import ConvictionVoting from "./components/ConvictionVoting.js";
+import TimeLine from "./components/TimeLine.js";
 class App extends Component {
+  constructor(props) {
+    super();
+
+    this.state = {
+      proposal: { name: "Pay 1000 to X" }
+    };
+  }
+
+  timeChanged(e) {
+    console.log("app: time changed");
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {/* <TimeLine
+          onTimeChange={e => {
+            this.timeChanged(e);
+          }}
+        /> */}
+        <ConvictionVoting proposal={this.state.proposal} />
+
       </div>
     );
   }
---
+++ src/components/ConvictionVoting.js
@@ -0,0 +1,103 @@
+import React, { Component } from "react";
+import { Line } from "react-chartjs-2";
+import convictionlib from "./convictionlib.js";
+
+class Me extends Component {
+  constructor(props) {
+    super();
+
+    this.state = {
+      proposal: props.proposal,
+      alpha: 90
+    };
+  }
+
+  componentWillMount() {
+    this.recalc();
+  }
+
+  recalc() {
+    const a = this.state.alpha / 100;
+    const D = 10;
+    let y = 0;
+    let x = 1e18;
+    let labels = [];
+    let data = [];
+
+    for (let t = 0; t < 100; t++) {
+      let y1 = convictionlib.getConviction(a, D, y, x, t);
+      labels.push(t);
+      data.push(y1);
+    }
+    //debugger;
+    this.setState({
+      plot: {
+        labels: labels,
+        datasets: [
+          {
+            fill: false,
+            lineTension: 0.1,
+            backgroundColor: "rgba(75,192,192,0.4)",
+            borderColor: "rgba(75,192,192,1)",
+            borderCapStyle: "butt",
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: "miter",
+            pointBorderColor: "rgba(75,192,192,1)",
+            pointBackgroundColor: "#fff",
+            pointBorderWidth: 1,
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: "rgba(75,192,192,1)",
+            pointHoverBorderColor: "rgba(220,220,220,1)",
+            pointHoverBorderWidth: 2,
+            pointRadius: 1,
+            pointHitRadius: 10,
+
+            label: "total conviction",
+            data: data
+          }
+        ]
+      }
+    });
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <section className="hero is-info welcome is-small">
+          <div className="hero-body">
+            <div className="container">
+              <h1 className="title">Proposal : {this.state.proposal.name}</h1>
+            </div>
+          </div>
+          Alpha= {this.state.alpha}
+          <input
+            class="slider is-fullwidth is-large is-danger is-circle"
+            step="1"
+            min="0"
+            max="100"
+            value={this.state.alpha}
+            type="range"
+            onChange={e => {
+              this.setState({ alpha: e.target.value });
+              this.recalc();
+            }}
+          />
+        </section>
+
+        <div className="card">
+          <header className="card-header">
+            <p className="card-header-title">Conviction chart</p>
+          </header>
+          <div className="card-content">
+            <div className="content">
+              {this.state.plot && <Line data={this.state.plot} />}
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Me;
