19821f4ad4d5e5226a2ce167e518c5442107ad76
Stefaan Ponnet <stefaan@ponnet.com>
2019-04-08 08:56:39 +0200

    Updates

---
+++ precache-manifest.141969cbb73bb9a8f439ce0626d00062.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cs-sim/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c5807e3d4635c7c4b6d2",
+    "url": "/cs-sim/static/js/runtime~main.a2a60435.js"
+  },
+  {
+    "revision": "2888339f3afa512064c8",
+    "url": "/cs-sim/static/js/main.e7e11178.chunk.js"
+  },
+  {
+    "revision": "791f42abdc7512db36ab",
+    "url": "/cs-sim/static/js/2.ccb4c09a.chunk.js"
+  },
+  {
+    "revision": "2888339f3afa512064c8",
+    "url": "/cs-sim/static/css/main.71f70c8a.chunk.css"
+  },
+  {
+    "revision": "791f42abdc7512db36ab",
+    "url": "/cs-sim/static/css/2.ba3e89f1.chunk.css"
+  },
+  {
+    "revision": "701d60cf69749a814af3236792e4133d",
+    "url": "/cs-sim/index.html"
+  }
+];
